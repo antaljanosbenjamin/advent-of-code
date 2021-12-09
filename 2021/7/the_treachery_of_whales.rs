694861eb533859c63b5530d1969898dd701bcbbb
János Benjamin Antal <benjamin.antal@memgraph.io>
2021-12-09 21:23:50 +0100

    Add better solution for day 7

--- 2021/7/the_treachery_of_whales.rs
+++ 2021/7/the_treachery_of_whales.rs
@@ -52,14 +52,53 @@ fn part2(crabs: &HashMap</*pos*/ usize, /*count*/ usize>) -> usize {
     })
 }
 
+// Based on https://github.com/Lalica/Advent-of-Code/blob/master/AoC2021/solutions/day07.py
+fn part1_better(crabs: &Vec<usize>) -> usize {
+    let sorted_crabs = {
+        let mut clone = crabs.clone();
+        clone.sort();
+        clone
+    };
+
+    let middle_index = (sorted_crabs.len() + 1) / 2;
+    let middle_pos = sorted_crabs[middle_index];
+    sorted_crabs
+        .iter()
+        .fold(0, |acc, &pos| distance(pos, middle_pos) + acc)
+}
+
+// Based on https://cdn.discordapp.com/attachments/541932275068174359/917782745894256640/crab-submarines.pdf
+fn part2_better(crabs: &Vec<usize>) -> usize {
+    let mean = crabs.iter().sum::<usize>() / crabs.len();
+    let solve = |mean| {
+        crabs
+            .iter()
+            .map(|pos| {
+                let dist = distance(mean, *pos);
+                (dist.pow(2) + dist + 1) / 2
+            })
+            .sum::<usize>()
+    };
+    solve(mean).min(solve(mean + 1))
+}
+
 fn main() {
     let file_content = fs::read_to_string("2021/7/input.txt").expect("Cannot read input file");
-    let mut crabs: HashMap</*pos*/ usize, /*count*/ usize> = HashMap::new();
-    file_content
+    let crabs_vec = file_content
         .split(',')
         .map(|str_num| str_num.parse::<usize>().expect("Invalid input"))
-        .for_each(|days| *crabs.entry(days).or_default() += 1);
+        .collect::<Vec<usize>>();
+
+    let crabs = crabs_vec.iter().fold(
+        HashMap::</*pos*/ usize, /*count*/ usize>::new(),
+        |mut crabs, &pos| {
+            *crabs.entry(pos).or_default() += 1;
+            crabs
+        },
+    );
 
     print_solution(1, part1(&crabs));
+    print_solution(1, part1_better(&crabs_vec));
     print_solution(2, part2(&crabs));
+    print_solution(2, part2_better(&crabs_vec));
 }
